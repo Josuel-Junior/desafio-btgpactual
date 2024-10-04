ef452427683112b4c6c63594ccda3fb43e4d0c69
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-10-04 15:20:29 -0300

    created config of file rabbitmq

---
+++ src/main/java/desafio/btgpactual/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,10 @@
+package desafio.btgpactual.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUAEU = "btg-pactual-order-created";
+
+}
